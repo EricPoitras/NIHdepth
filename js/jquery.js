058700e176acfad5faeca141408ce93d2263e9f5
EricPoitras <EricPoitras@users.noreply.github.com>
2018-05-23 22:58:25 -0600

    Added sub-page (VASE-R) for module

--- js/jquery.js
+++ js/jquery.js
@@ -10,5 +10,77 @@ $(document).ready(function(){
 
    // jQuery methods go here...
     console.log("Hello world!");
-
+    
+    
+    // Modules page 
+    $("#vasercard").hover(function(){
+        $(this).css("background-color", "lightgray");
+    },function(){
+        $(this).css("background-color", "white");
+    });
+    
+    $("#vasercard").click(function(){
+        window.open("modules/vase-r.html","_blank"); 
+    });
+    
+    // Sub-module page
+    $(".list-group-navcus .list-group-item").hover(function(){
+        $(this).addClass("activecus");
+    },function(){
+        $(this).removeClass("activecus");
+    });
+    
+    $(".list-group-navcus .list-group-item").click(function(){
+        $(".list-group-navcus .list-group-item").removeClass("activecus");
+        $(".list-group-navcus .list-group-item").removeClass("activecus2"); 
+    });
+    
+    $(".list-group-navcus .list-group-item:nth-child(1)").click(function(){
+        $(".list-group-1").removeClass("invisiblecus");
+        $(".list-group-1").addClass("visiblecus");
+        $(".list-group-2").removeClass("visiblecus");
+        $(".list-group-2").addClass("invisiblecus");
+        $(".list-group-3").removeClass("visiblecus");
+        $(".list-group-3").addClass("invisiblecus");
+        $(".list-group-4").removeClass("visiblecus");
+        $(".list-group-4").addClass("invisiblecus");
+        $(this).addClass("activecus2");
+    });
+    
+     $(".list-group-navcus .list-group-item:nth-child(2)").click(function(){
+        $(".list-group-2").removeClass("invisiblecus");
+        $(".list-group-2").addClass("visiblecus");
+        $(".list-group-1").removeClass("visiblecus");
+        $(".list-group-1").addClass("invisiblecus");
+        $(".list-group-3").removeClass("visiblecus");
+        $(".list-group-3").addClass("invisiblecus");
+        $(".list-group-4").removeClass("visiblecus");
+        $(".list-group-4").addClass("invisiblecus");
+         $(this).addClass("activecus2");
+    });
+    
+     $(".list-group-navcus .list-group-item:nth-child(3)").click(function(){
+        $(".list-group-3").removeClass("invisiblecus");
+        $(".list-group-3").addClass("visiblecus");
+        $(".list-group-2").removeClass("visiblecus");
+        $(".list-group-2").addClass("invisiblecus");
+        $(".list-group-1").removeClass("visiblecus");
+        $(".list-group-1").addClass("invisiblecus");
+        $(".list-group-4").removeClass("visiblecus");
+        $(".list-group-4").addClass("invisiblecus");
+         $(this).addClass("activecus2");
+    });
+    
+     $(".list-group-navcus .list-group-item:nth-child(4)").click(function(){
+        $(".list-group-4").removeClass("invisiblecus");
+        $(".list-group-4").addClass("visiblecus");
+        $(".list-group-2").removeClass("visiblecus");
+        $(".list-group-2").addClass("invisiblecus");
+        $(".list-group-3").removeClass("visiblecus");
+        $(".list-group-3").addClass("invisiblecus");
+        $(".list-group-1").removeClass("visiblecus");
+        $(".list-group-1").addClass("invisiblecus");
+         $(this).addClass("activecus2");
+    });
+    
 });
